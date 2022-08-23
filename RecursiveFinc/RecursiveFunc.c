a3b7db8ba45191883a40eadead2cac9edbd4e5fb
vipstard <vipstar_d@naver.com>
2022-08-23 23:47:30 +0900

    Fibo

---
+++ RecursiveFinc/RecursiveFunc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void Recursive(int num) 
+{
+	if (num <= 0) return; //Àç±ÍÀÇ Å»ÃâÁ¶°Ç
+
+	printf("Recursive call!! %d \n", num);
+	Recursive(num - 1);
+}
+
+int main(void)
+{
+	Recursive(3);
+	return 0;
+}
