08a93cc57f9e981b460c6f521d6a67a3a65cdf77
vipstard <vipstar_d@naver.com>
2022-08-31 21:36:12 +0900

    list

--- ListMain/ArrayList.h
+++ ListMain/ArrayList.h
@@ -1,31 +1,27 @@
-#ifndef __ARRAY_LIST_H__
+#ifndef  __ARRAY_LIST_H__
 #define __ARRAY_LIST_H__
 
-#define TRUE	1
-#define FALSE	0
+#define TRUE 1 // '참'을 표현하기위한 매크로 정의
+#define False 0 // '거짓'을 표현하기 위한 매크로 정의
 
-/*** ArrayList의 정의 ****/
-#define LIST_LEN	100
-typedef int LData;
+#define LIST_LEN 100
+typedef int LData; // LData에 대한 typedef 선언
 
-typedef struct __ArrayList
+typedef struct __ArrayList // 배열 기반 리스트를 정의한 구조체
 {
-	LData arr[LIST_LEN];
-	int numOfData;
-	int curPosition;
+	LData arr[LIST_LEN]; // 리스트의 저장소인 배열
+	int numOfData; // 저장된 데이터의 수
+	int curPosition; // 데이터 참조위치를 기록
 } ArrayList;
 
-
-/*** ArrayList와 관련된 연산들 ****/
 typedef ArrayList List;
 
-void ListInit(List * plist);
-void LInsert(List * plist, LData data);
+void ListInit(List* plist); // 초기화
+void LInsert(List* plist, LData data); // 데이터 저장
 
-int LFirst(List * plist, LData * pdata);
-int LNext(List * plist, LData * pdata);
+int LFirst(List* plist, LData* pdata); // 첫 데이터 참조
+int LNext(List* plist, LData* pdata); // 두 번째 이후 데이터 참조
 
-LData LRemove(List * plist);
-int LCount(List * plist);
-
-#endif
+LData LRemove(List* plist); // 참조한 데이터 삭제
+int LCount(List* plist); // 저장된 데이터의 수 반환
+#endif // ! __ARRAY_LIST_H__
